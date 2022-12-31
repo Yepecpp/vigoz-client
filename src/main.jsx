a8fda2fb763e62f8a0f8d5ecb84ae1113a4bf632
yepecpp <jhonmiguel.1@hotmail.com>
2022-12-30 20:20:54 -0400

    changed styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom/client';
 //scss
 import App from './App';
 //scss
-import './styles/Login.scss';
-import './styles/FooterDash.scss';
-import './styles/Letters.scss';
-import './styles/NavbarDash.scss';
-
+import './styles/Login.p.scss';
+import './styles/Dashboard.p.scss';
 const Render = () => {
   ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
