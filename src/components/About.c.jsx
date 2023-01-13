30a90715adebdf3dc15ac4c19b0029801057dfa1
FritoLay23 <alexanderlop790@gmail.com>
2023-01-12 23:36:53 -0400

    advance of home and about

--- src/components/About.c.jsx
+++ src/components/About.c.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BsShop } from 'react-icons/bs';
+import { GiIcebergs } from 'react-icons/gi';
 
 const Aboutc = () => {
   return (
@@ -7,24 +7,26 @@ const Aboutc = () => {
       <div className="contenedor_sbn">
         <div className="contenedor_sbn_1">
           <h1>
-            <BsShop />
+            <GiIcebergs />
           </h1>
         </div>
         <div className="contenedor_sbn_2">
-          <h1>Acerca de Nosotros</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi
-            dicta voluptatem rem. Delectus asperiores commodi fugit quisquam
-            eius blanditiis voluptas, odio veniam sequi, modi doloribus quia
-            rerum exercitationem quaerat incidunt beatae possimus, voluptatum
-            sit hic pariatur. Rem cupiditate omnis asperiores numquam ipsam
-            ullam impedit vero odit, dolore id obcaecati a aut eaque eum eius,
-            doloremque ea. Voluptas ducimus, nemo sint pariatur quaerat
-            blanditiis accusantium rerum nam neque voluptatum aut dicta vero
-            obcaecati soluta magni consequatur alias iusto eligendi. Amet cum
-            delectus omnis ipsam ex atque, animi doloremque nesciunt voluptas
-            vitae?
-          </p>
+          <div className="contenedor_sbn_2_1">
+            <h1>Acerca de Nosotros</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Excepturi dicta voluptatem rem. Delectus asperiores commodi fugit
+              quisquam eius blanditiis voluptas, odio veniam sequi, modi
+              doloribus quia rerum exercitationem quaerat incidunt beatae
+              possimus, voluptatum sit hic pariatur. Rem cupiditate omnis
+              asperiores numquam ipsam ullam impedit vero odit, dolore id
+              obcaecati a aut eaque eum eius, doloremque ea. Voluptas ducimus,
+              nemo sint pariatur quaerat blanditiis accusantium rerum nam neque
+              voluptatum aut dicta vero obcaecati soluta magni consequatur alias
+              iusto eligendi. Amet cum delectus omnis ipsam ex atque, animi
+              doloremque nesciunt voluptas vitae?
+            </p>
+          </div>
         </div>
       </div>
     </div>
