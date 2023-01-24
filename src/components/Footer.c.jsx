7a2143c6c3cf24c331578c23307b374af3b6d82e
yepecpp <jhonmiguel.1@hotmail.com>
2023-01-23 21:02:18 -0400

    vite 3 to 4

--- src/components/Footer.c.jsx
+++ src/components/Footer.c.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import {
   FaFacebook,
   FaTwitter,
   FaYoutube,
-  FaGooglePlusG,
+  // FaGooglePlusG,
   FaGithub,
 } from 'react-icons/fa';
 
@@ -15,32 +14,46 @@ function Footeri() {
         <div className="contenedor_footer">
           <h3 className="footer_h3">Hielo Vigoz</h3>
           <p className="footer_descripcion">
-            Dirección: Calle Francisco Henríquez y Carvajal #140, sector San Carlos 
-            Fundación de empresa: 15 de diciembre del año 2015 
-            RNC: 1-3209235-1
-            Teléfono y Ws: 809-221-0444
-            Precio del hielo por unidad: 35
-            Horario laboral con delivery: de lunes a Sábado de 7:30 am a 4:00pm y los domingos de 9:00 am a 1:00 pm
+            Dirección: Calle Francisco Henríquez y Carvajal #140, sector San
+            Carlos Fundación de empresa: 15 de diciembre del año 2015 RNC:
+            1-3209235-1 Teléfono y Ws: 809-221-0444 Precio del hielo por unidad:
+            35 Horario laboral con delivery: de lunes a Sábado de 7:30 am a
+            4:00pm y los domingos de 9:00 am a 1:00 pm
           </p>
           <ul className="socials">
             <li>
-              
-              <a href="https://es-la.facebook.com/login/device-based/regular/login/?login_attempt=1" target="_blank">
+              <a
+                href="https://es-la.facebook.com/login/device-based/regular/login/?login_attempt=1"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <FaFacebook />
               </a>
             </li>
             <li>
-              <a href="https://twitter.com/?lang=es" target="_blank">
+              <a
+                href="https://twitter.com/?lang=es"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <FaTwitter />
               </a>
             </li>
             <li>
-              <a href="https://www.youtube.com/" target="_blank">
+              <a
+                href="https://www.youtube.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <FaYoutube />
               </a>
             </li>
             <li>
-              <a href="https://github.com/Yepecpp" target="_blank">
+              <a
+                href="https://github.com/Yepecpp"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <FaGithub />
               </a>
             </li>
