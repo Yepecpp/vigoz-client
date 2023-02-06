053c91a4c1aa1a58d02ba61d2a00a05e335eda18
yepecpp <jhonmiguel.1@hotmail.com>
2023-02-06 14:57:51 -0400

    added proveders datagrid

--- src/components/Provedores.c.jsx
+++ src/components/Provedores.c.jsx
@@ -1,9 +1,36 @@
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
 import Udatagrid from './datagrid/Udatagrid.c.jsx';
 import AgregarData from './AgregarData.c.jsx';
-
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { AxiosClient } from '../libs/axios';
+import moment from 'moment';
+import Loading from './Loading.c';
 const Provedores = () => {
   const [isOpened, SetisOpened] = useState(false);
+  const [search, SetSearch] = useState('');
+  const queryClient = useQueryClient();
+  const providerQuery = useQuery({
+    queryKey: ['providers'],
+    queryFn: async () => {
+      const axios = AxiosClient();
+      let response = await axios.get(
+        '/providers' + `${search !== '' ? `?name=${search}` : ''}`
+      );
+      response.data.providers.forEach((provider) => {
+        provider.user = provider.user ? provider.user.name : 'No user';
+        provider.createdAt = moment(provider.createdAt).format('YYYY-MM-DD');
+        provider.updatedAt = moment(provider.updatedAt).format('YYYY-MM-DD');
+      });
+      return response.data;
+    },
+  });
+  useEffect(() => {
+    if (!queryClient.isFetching(['providers']))
+      setTimeout(() => {
+        queryClient.invalidateQueries(['providers']);
+        providerQuery.refetch();
+      }, 1000);
+  }, [search]);
   const GridProps = {
     columns: [
       { field: 'id', headerName: 'ID', width: 100 },
@@ -11,15 +38,30 @@ const Provedores = () => {
       { field: 'email', headerName: 'Email', width: 230 },
       { field: 'phone', headerName: 'Phone', width: 230 },
       { field: 'address', headerName: 'Address', width: 230 },
-      { field: 'description', headerName: 'Description', width: 230 },
+      {
+        field: 'description',
+        headerName: 'Description',
+        width: 230,
+      },
     ],
-    rows: [],
+    rows: providerQuery?.data?.providers,
   };
 
   return (
     <div className="container_formProvedores">
-      <AgregarData isOpened={isOpened} SetisOpened={SetisOpened} />
-      <Udatagrid data={GridProps} />
+      <AgregarData
+        isOpened={isOpened}
+        SetisOpened={SetisOpened}
+        handleChange={(e) => SetSearch(e.target.value)}
+        search={search}
+      />
+      {providerQuery.isLoading ? (
+        <Loading />
+      ) : providerQuery.error ? (
+        <div>error</div>
+      ) : (
+        <Udatagrid data={GridProps} />
+      )}
     </div>
   );
 };
