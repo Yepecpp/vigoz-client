cccbdf4c3a94cdd2eaca05f9a222053719ee080b
yepecpp <jhonmiguel.1@hotmail.com>
2023-02-19 21:56:27 -0400

    added dropdown menu

--- src/components/popups/Perfil.popup.jsx
+++ src/components/popups/Perfil.popup.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react';
 import { Button, Box } from '@mui/material';
-import { useAuthContext } from '../../contexts/Auth';
 import { AxiosClient } from '../../libs/axios';
 const PerfilPopup = ({ isOpened, setIsOpened }) => {
-  const [auth] = useAuthContext();
   const [passw, Setpassw] = useState({
     old: '',
     new: '',
@@ -18,62 +16,63 @@ const PerfilPopup = ({ isOpened, setIsOpened }) => {
       alert('Las contraseñas no coinciden');
       return;
     }
-    const response = await AxiosClient().put('/auth/password', {
-      password: passw,
-    });
-    if (response.status === 200) {
-      alert('Contraseña cambiada con exito');
-      setIsOpened(false);
-      return;
+    {
+      const response = await AxiosClient().put('/auth/password', {
+        password: passw,
+      });
+      if (response.status === 200) {
+        alert('Contraseña cambiada con exito');
+        setIsOpened(false);
+        return;
+      }
+      alert('Error al cambiar la contraseña');
     }
-    alert('Error al cambiar la contraseña');
-  };
-  return (
-    <div id="myModal" className="modal">
-      <div className="modal-content">
-        <span className="closeperfil" onClick={() => setIsOpened(!isOpened)}>
-          &times;
-        </span>
-        <p>Cambiar Contraseña</p>
-        <div className="content-contra">
-          <label htmlFor="" className="info_popup">
-            Contraseña actual
-          </label>
-          <input
-            type="text"
-            value={passw.old}
-            onChange={(e) => Setpassw({ ...passw, old: e.target.value })}
-          />
-          <label htmlFor="" className="info_popup">
-            Contraseña nueva
-          </label>
-          <input
-            type="text"
-            value={passw.new}
-            onChange={(e) => Setpassw({ ...passw, new: e.target.value })}
-          />
-          <label htmlFor="" className="info_popup">
-            Confirmar contraseña
-          </label>
-          <input
-            type="text"
-            value={passw.confirm}
-            onChange={(e) => Setpassw({ ...passw, confirm: e.target.value })}
-          />
-          <Box mt={3}>
-            <Button
-              variant="outlined"
-              type="submit"
-              className="btn_contra"
-              onClick={sendData}
-            >
-              Guardar
-            </Button>
-          </Box>
+    return (
+      <div id="myModal" className="modal">
+        <div className="modal-content">
+          <span className="closeperfil" onClick={() => setIsOpened(!isOpened)}>
+            &times;
+          </span>
+          <p>Cambiar Contraseña</p>
+          <div className="content-contra">
+            <label htmlFor="" className="info_popup">
+              Contraseña actual
+            </label>
+            <input
+              type="text"
+              value={passw.old}
+              onChange={(e) => Setpassw({ ...passw, old: e.target.value })}
+            />
+            <label htmlFor="" className="info_popup">
+              Contraseña nueva
+            </label>
+            <input
+              type="text"
+              value={passw.new}
+              onChange={(e) => Setpassw({ ...passw, new: e.target.value })}
+            />
+            <label htmlFor="" className="info_popup">
+              Confirmar contraseña
+            </label>
+            <input
+              type="text"
+              value={passw.confirm}
+              onChange={(e) => Setpassw({ ...passw, confirm: e.target.value })}
+            />
+            <Box mt={3}>
+              <Button
+                variant="outlined"
+                type="submit"
+                className="btn_contra"
+                onClick={sendData}
+              >
+                Guardar
+              </Button>
+            </Box>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  };
 };
-
 export default PerfilPopup;
--- src/components/sidebar/Dropdown.c.jsx
+++ src/components/sidebar/Dropdown.c.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FaTruck } from 'react-icons/fa';
 
-const Dropdown = () => {
+const Dropdown = ({ title, Icon, childs }) => {
+  const [click, setClick] = useState(false);
   return (
-      <li className="item_sidebar">
-        <div className="link_sidebar">
-          <FaTruck />
-          <p className="container_userSidebar">Distribución</p>
-        </div>
-        <ul className="dropdown">
-          <li className="dropdown_item">
-            <Link to="/Dashboard/Distribucion">Distribucion 1</Link>
-          </li>
-          <li className="dropdown_item">
-            <Link to="/Dashboard/Distribucion">Distribucion 2</Link>
-          </li>
-        </ul>
-      </li>
+    <li className="item_sidebar" onClick={() => setClick(!click)}>
+      <div className="link_sidebar">
+        <Icon />
+        <p className="container_userSidebar">{title}</p>
+      </div>
+      {click
+        ? childs.map((Child, index) => {
+            return (
+              <ul className="dropdown" key={index}>
+                <li className="dropdown_item">
+                  <Child.icon />
+                  <Link to={Child.path}>{Child.title}</Link>
+                </li>
+              </ul>
+            );
+          })
+        : null}
+    </li>
   );
 };
 
 export default Dropdown;
+/*
+<ul className="dropdown">
+        <li className="dropdown_item">
+          <Link to="/Dashboard/Distribucion">Distribucion 1</Link>
+        </li>
+        <li className="dropdown_item">
+          <Link to="/Dashboard/Distribucion">Distribucion 2</Link>
+        </li>
+      </ul>*/
